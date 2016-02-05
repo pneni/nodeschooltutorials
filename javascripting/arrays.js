6de9581d50b72779447e1af708564a033f27cff4
Padma Neni <padmaneni@Padma.local>
2016-02-05 13:27:57 +0000

    practice on Arrays

---
+++ javascripting/arrays.js
@@ -0,0 +1,11 @@
+/*
+	## ARRAYS (Exercise 11 of 19)  
+   
+  An array is a list of values. Here's an example:  
+   
+     var pets = ['cat', 'dog', 'rat'];  
+*/
+
+var pizzaToppings = ["tomato sauce", "cheese", "pepperoni"];
+
+console.log(pizzaToppings);
--- javascripting/if-statement.js
+++ javascripting/if-statement.js
@@ -23,6 +23,6 @@
 var fruit = 'orange';
 if (fruit.length>5){
 	console.log("The fruit name has more than five characters.");
-} else {
+} else 
 	console.log( "The fruit name has five characters or less.");
 }
