da8f5d8cd45c08568ce9a11463fdf3b0aeb041af
Padma Neni <padmaneni@Padma.local>
2016-02-05 13:35:15 +0000

    practice on Objects

---
+++ javascripting/object-properties.js
@@ -0,0 +1,29 @@
+/*
+## OBJECT PROPERTIES (Exercise 16 of 19)  
+   
+  You can access and manipulate object properties –– the keys and values  
+  that an object contains –– using a method very similar to arrays.  
+   
+  Here's an example using square brackets:  
+   
+     var example = {  
+       pizza: 'yummy'  
+     };  
+       
+     console.log(example['pizza']);  
+   
+  The above code will print the string 'yummy' to the terminal.  
+   
+  Alternately, you can use dot notation to get identical results:  
+   
+     example.pizza;  
+       
+     example['pizza'];  
+   
+  The two lines of code above will both return yummy. 
+*/
+var food = {
+	types: 'only pizza'
+};
+
+console.log(food.types);
---
+++ javascripting/objects.js
@@ -0,0 +1,22 @@
+
+/*
+## OBJECTS (Exercise 15 of 19)  
+   
+  Objects are lists of values similar to arrays, except values are  
+  identified by keys instead of integers.  
+   
+  Here is an example:  
+   
+     var foodPreferences = {  
+       pizza: 'yum',  
+       salad: 'gross'  
+     };  
+
+*/
+var  pizza = {
+	toppings: ['cheese', 'sauce', 'pepperoni'],
+	crust :'deep dish',
+	serves :2
+};
+
+console.log(pizza);
